a5100619d4dba344e92083e236a0901b696d9707
Smruti2506 <ksmruti8010@gmail.com>
2025-04-19 23:07:23 +0530

    BICT-4 :Select image from file for compression frontend

--- frontend/src/components/ImageCompressor.jsx
+++ frontend/src/components/ImageCompressor.jsx
@@ -130,12 +130,12 @@ function ImageCompressor() {
         />
       </div>
 
-      {/* <div style={{ marginTop: "15px" }}>
+       <div style={{ marginTop: "15px" }}>
         <button onClick={handleUpload}>Compress Images</button>{" "}
         <button onClick={handleClear} style={{ marginLeft: "10px" }}>
           Clear
         </button>
-      </div> */}
+      </div> 
 
       {/* ✅ Modal for results 
       {showModal && (
