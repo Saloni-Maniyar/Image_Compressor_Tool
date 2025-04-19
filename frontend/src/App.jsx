bd4414077c2417286445410de37b8d982f8d1a3c
Smruti2506 <ksmruti8010@gmail.com>
2025-04-19 15:42:59 +0530

    BICT-5 :Priview of the selected image  frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+// frontend/src/App.jsx
+import React from "react";
+import ImageCompressor from "./components/ImageCompressor";
+
+function App() {
+  return <ImageCompressor />;
+}
+
+export default App;
